29c2a31699b179481db048ae2ee765cd40c1fac3
FredericForest <98435087+FredericForest@users.noreply.github.com>
2022-02-22 21:58:24 -0500

    Add files via upload

--- Logiciel/Vision/vision_windows.py
+++ Logiciel/Vision/vision_windows.py
@@ -2,38 +2,35 @@ import numpy as np
 from PIL import Image
 import cv2
 
-
 def vision():
+    path = r"pic.jpeg"
     cap = cv2.VideoCapture(0)
+
+    # Check if the webcam is opened correctly
     if not cap.isOpened():
         raise IOError("Cannot open webcam")
-    # while True:
     ret, frame = cap.read()
+    cv2.imwrite(path, frame)
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
 
-    # frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    #     c = cv2.waitKey(1)
-    #     if c == 27:
-    #         break
-
-    cap.release()
-    # cv2.destroyAllWindows()
-    im = frame
+    im = Image.open(path).convert('L') #.resize([10000, 10000])
     pixel = im.load()
-    x = 0
-    y = 0
-    nb = 0
+    x =0
+    y =0
+    nb =0
     for i in range(im.size[0]):
         for j in range(im.size[1]):
-            if pixel[i, j] > 150:
-                im.putpixel([i, j], 255)
+            if pixel[i,j] > 150:
+                im.putpixel([i,j], 255)
                 x += i
                 nb += 1
                 y += j
             else:
-                im.putpixel([i, j], 0)
+                im.putpixel([i,j], 0)
 
-    x = int(x / nb)
-    y = int(y / nb)
+    x = int(x/nb)
+    y = int(y/nb)
     coord = [x, y]
     return coord
+
 print(vision())
