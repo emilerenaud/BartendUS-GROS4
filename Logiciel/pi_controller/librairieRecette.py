08f08264dcf9eebf15523d4a183465b9b25d7ef4
antoine landry <antoinelandry99@gmail.com>
2022-03-16 21:53:02 -0400

    implementation class ingredient ajout fonction recette correction bug

--- Logiciel/pi_controller/librairieRecette.py
+++ Logiciel/pi_controller/librairieRecette.py
@@ -1,3 +1,81 @@
+class ingredient_dispo():
+
+
+    def __init__(self):
+        self.list_ingredient = []
+        self.list_quantite = []
+        self.list_position=[]
+        self.lireIngredientDispo()
+
+    def lireIngredientDispo(self):
+
+        f = open("repertoireIngredient.txt", 'r', encoding="utf-8")
+        #exemple typo : Vodka : 750 , position : 8
+        for line in f:
+
+            chaine=line.split(",")
+            ingredientQuantite=chaine[0].split(":")
+            self.list_ingredient.append( ingredientQuantite[0])
+            self.list_quantite.append( ingredientQuantite[1])
+
+            textePosition = chaine[1].split(":")
+            self.list_position.append(textePosition[1])
+
+        f.close()
+        return
+
+    def get_list_ingredient(self):
+        return self.list_ingredient
+
+    def get_list_ingredient_string(self):
+        list_chaine=[]
+        for i in range(len(self.list_ingredient)):
+            chaine =self.list_alcool[i] + ":" + str(self.list_quantite[i]+"ml position : "+str(self.list_position[i]))
+            list_chaine.append(chaine)
+        return list_chaine
+
+    def __str__(self):
+        chaine=""
+        for i in range(len(self.list_ingredient)):
+                chaine =chaine+ self.list_ingredient[i] + ":" + str(self.list_quantite[i]) + "ml, Position:" + str(self.list_position[i])
+                if chaine[-1]!="\n":
+                    chaine=chaine+"\n"
+        return chaine
+
+    def supprimerSauvegarde(self,index):
+        chaineAsupprimer=self.list_ingredient[index] + ":" + str(self.list_quantite[index])+", position : "+str(self.list_position[index])
+        f= open("repertoireIngredient.txt", 'r', encoding="utf-8")
+        lines = f.readlines()
+        f = open("repertoireIngredient.txt", 'w', encoding="utf-8")
+        for line in lines:
+            if line.strip("\n") != chaineAsupprimer:
+                f.write(line)
+
+    def supprimerIngredient(self,index):
+        self.supprimerSauvegarde(index)
+        self.list_ingredient.pop(index)
+        self.list_quantite.pop(index)
+        self.list_position.pop(index)
+        return
+
+
+    def sauvegardeIngredient(self, ingredient, quantite, position):
+        f = open("repertoireIngredient.txt", 'a', encoding="utf-8")
+        f.write("\n"+ingredient + ":" + str(quantite)+", position : "+str(position))
+        f.close()
+        return
+
+    def ajouterIngredient(self, ingredient, quantite, position):
+
+        self.list_ingredient.append(ingredient)
+        self.list_quantite.append(quantite)
+        self.list_position.append(position)
+        self.sauvegardeIngredient(ingredient, quantite, position)
+        return
+
+
+
+
 
 
 class recette() :
@@ -21,6 +99,12 @@ class recette() :
 
     def getlistQuantite(self):
         return self.list_quantite
+
+    def afficherIngredient(self):
+        chaine = ""
+        for i in range(len(self.list_alcool)):
+            chaine = chaine +str(i+1)+". " + self.list_alcool[i] + ", Quantité:" + str(self.list_quantite[i])+"oz \n"
+        return chaine
 
     def __str__(self):
         chaine=self.titre
@@ -49,13 +133,18 @@ class livreRecette():
             chaine = "" + chaine + recette.__str__() + "\n"
         return chaine
 
+    def list_recette_dispo_string(self):
+        list_chaine = []
+        for recette in self.list_recette_dispo:
+            list_chaine.append(recette.getTitre)
+        return list_chaine
 
     def getRecette(self,index):
         return self.list_recette[index]
 
     def sauvegardeRecette(self,recette):
         f = open("repertoireRecette.txt", 'a', encoding="utf-8")
-        f.write(recette.__str__())
+        f.write("\n"+recette.__str__())
         f.close()
         return
 
@@ -69,11 +158,7 @@ class livreRecette():
 
         f = open("repertoireRecette.txt", 'r', encoding="utf-8")
         list_recette=[]
-        # debut=False
-        # while(debut is not True):
-        #     if(f.read() != 'G'):
-        #         debut=True
-        #         print("go")
+
         for line in f:
             list_alcool = []
             list_quantite = []
@@ -90,7 +175,10 @@ class livreRecette():
 
         return list_recette
 
-    def update_recette_dispo(self,list_alcool):
+    def update_recette_dispo(self,ingredient_dispo):
+    #TO DO
+    # GERER LOWER CASE
+        list_ingredient=ingredient_dispo.get_list_ingredient()
         list_recette_dispo=[]
 
         for recette in self.list_recette:
@@ -98,7 +186,7 @@ class livreRecette():
             nbIngredient=len(recette.getlistAlcool())
             for i in range(nbIngredient):
                 if trouver == i:
-                    for alcool in list_alcool:
+                    for alcool in list_ingredient:
                         if alcool==recette.getlistAlcool()[i]:
                             if i == nbIngredient-1:
                                 list_recette_dispo.append(recette)
@@ -110,12 +198,24 @@ class livreRecette():
         return self.list_recette_dispo
 
 ##test
-list_alcool=["alcool","alcool2","lime","swince"]
+livreIngredient=ingredient_dispo()
+# print(livreIngredient.list_ingredient[1])
+# print("recette dispo :",livreIngredient)
+# livreIngredient.supprimerIngredient(2)
+# print("recette dispo :",livreIngredient)
 #init livre
 livre=livreRecette()
+# print(livre)
+livre.update_recette_dispo(livreIngredient)
+# print(livre.list_recette_dispo[0].afficherIngredient())
+livreIngredient.ajouterIngredient("rhum",2,1)
+livreIngredient.ajouterIngredient("cola",2,1)
+print("livreIngredient : \n",livreIngredient)
+livre.ajouterRecette("bloodyhell",["bloody","tequila"],[6,9])
+
 print(livre)
-livre.update_recette_dispo(list_alcool)
-print("La/les recette(s) disponible(s) sont : \n",livre.afficherRecetteDispo())
+# print("La/les recette(s) disponible(s) sont : \n",livre.afficherRecetteDispo())
+
 
 
 
